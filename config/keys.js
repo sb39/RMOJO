3fc01acc2127abd97389f8bb191fc18a3d7e6f54
sb39 <biswassrijeet99@gmail.com>
2019-08-24 11:16:31 +0530

    iss res

---
+++ config/keys.js
@@ -0,0 +1,12 @@
+module.exports = {
+    google : {
+        clientID :  '545096457146-hcoeifsh3m7rf80er8dpkgtk1k19pia1.apps.googleusercontent.com',
+        clientSecret : 'eheXRVAuNPp9IWDLpNnPIOVo'
+    },
+    mongo : {
+        dbURL : 'mongodb+srv://test:test123@test-s8kcw.mongodb.net/test?retryWrites=true&w=majority'
+    },
+    session : {
+        sessionToken : 'iamaawesomeguy'
+    }
+};
